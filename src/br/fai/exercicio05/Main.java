e49760059b6d43989ffdd075b80c944a7367f04a
fernandinho <lfrfernandinho@gmail.com>
2020-03-07 10:58:48 -0300

    segundo commit

--- src/br/fai/exercicio05/Main.java
+++ src/br/fai/exercicio05/Main.java
@@ -1,10 +1,40 @@
 package br.fai.exercicio05;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+		
 
 	}
 
-}
+	private void start() {
+		
+		System.out.println("Enviando essa branch para o github");
+		usandoFor();
+		
+		}
+	
+	private void usandoFor() {
+		for (int i = 0; i < 5; i++) {
+			switch (i) {
+			case 1: System.out.println("Primeira condição");
+			break;
+			
+			case 4: System.out.println("Segunda condição");
+			break;
+			
+			default:
+				System.out.println("Não encontrou nenhuma condição");
+				break;
+			}
+	}
+	
+		
+	}
+	
+
+
+	
+	}
